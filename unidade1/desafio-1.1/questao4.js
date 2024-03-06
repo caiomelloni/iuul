24b698cc69e1d9afb7e3089e3cd2bf5a4a4c8532
Caio Melloni Dos Santos <ra167974@kraft.lab.ic.unicamp.br>
2024-03-05 23:08:40 -0300

    add questao4.js

--- unidade1/desafio-1.1/questao4.js
+++ unidade1/desafio-1.1/questao4.js
@@ -51,8 +51,8 @@ class Turma {
 			let aluno = {
 				matricula: key,
 				nome: value.nome,
-				p1: value.p1 == 0 ? "-" : value.p1,
-				p2: value.p2 == 0 ? "-" : value.p2,
+				p1: value.p1 == 0 ? "-" : value.p1.toFixed(1),
+				p2: value.p2 == 0 ? "-" : value.p2.toFixed(1),
 				nf: Number(((value.p1 + value.p2) / 2).toFixed(1))
 			}
 
@@ -64,9 +64,27 @@ class Turma {
 
 	imprimirAlunos() {
 		let alunos = this.#alunosSortedJsonArray()	
-		console.log(alunos)
+		let lenMaiorMatricula = 10
+		let lenMaiorNome = 5
+
+		for (let aluno of alunos) {
+			let lenMatricula = String(aluno.matricula).length
+			let lenNome = aluno.nome.length
+
+			if (lenMatricula > lenMaiorMatricula) lenMaiorMatricula = lenMatricula
+			if (lenNome > lenMaiorNome) lenMaiorNome = lenNome + 3
+		}
+
+		console.log("-".repeat(lenMaiorMatricula + lenMaiorNome + 12))
+		console.log("Matricula".padEnd(lenMaiorMatricula, " ") + "Nome".padEnd(lenMaiorNome) + "P1".padStart(4, " ") + "P2".padStart(4," ") + "NF".padStart(4, " "))
+		console.log("-".repeat(lenMaiorMatricula + lenMaiorNome + 12))
+
+		for (let aluno of alunos) {
+			console.log(String(aluno.matricula).padEnd(lenMaiorMatricula, " ") + aluno.nome.padEnd(lenMaiorNome) + String(aluno.p1).padStart(4, " ") + String(aluno.p2).padStart(4," ") + String(aluno.nf).padStart(4, " "))
+		}
 
 
+		console.log("-".repeat(lenMaiorMatricula + lenMaiorNome + 12))
 	}
 
 }
@@ -75,7 +93,7 @@ function main() {
 	let turma = new Turma()	
 
 	let ana = new Aluno(12345, "Ana de Almeida")
-	let bruno = new Aluno(123456, "Bruno Carvalho")
+	let bruno = new Aluno(23456, "Bruno Carvalho")
 	let fernanda = new Aluno(34567, "Fernanda Abreu")
 	let joao = new Aluno(45678, "Joao Santos")
 	let caio = new Aluno(777, "Caio Santos")
@@ -96,4 +114,6 @@ function main() {
 }
 
 
-main()
+if (require.main == module) {
+	main()
+}
