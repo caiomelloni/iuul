9b18a656e177964def4967b986482eb372c1d137
Caio Melloni Dos Santos <ra167974@beach.lab.ic.unicamp.br>
2024-03-06 13:59:12 -0300

    WIP: add alguns validadores

--- unidade1/desafio-1.1/questao5.js
+++ unidade1/desafio-1.1/questao5.js
@@ -20,6 +20,57 @@ async function lerCliente() {
 
 }
 
+class Cliente {
+
+	static erros = {
+		nome: "NOME",
+		cpf: "CPF",
+		dtNascimento: "DATANASCIMENTO",
+		renda: "RENDA",
+		estadoCivil: "ESTADOCIVIL",
+		dependentes: "DEPENDENTES"
+	}
+
+	constructor(nome, cpf, nascimento, renda, estadoCivil, dependentes) {
+		this.nome = nome
+		this.cpf = cpf
+		this.nascimento = nascimento
+		this.renda = renda
+		this.estadoCivil = estadoCivil
+		this.dependentes = dependentes
+	}
+
+	static #validarNome(nome) {
+		let nomeString = String(nome)
+		if (nomeString.length < 5) throw Cliente.erros.nome
+		return nomeString
+	}
+
+	static #validarCpf(cpf) {
+		let cpfNumber = Number(cpf)
+		if (cpfNumber == NaN || String(cpfNumber).length != 11) throw Cliente.erros.cpf
+		return cpfNumber
+	}
+
+	static #validarNascimento(nascimento) {
+		let [diaString, mesString, anoString] = nascimento.split("/")
+		let date = new Date(mesString + "/" + diaString + "/" + anoString)
+		if (isNaN(date)) throw Cliente.erros.dtNascimento
+		return date
+	}
+
+	static #validarRenda(renda) {
+		let rendaString = renda.replace(",", ".")
+		let renda = Number(Number(rendaString).toFixed(2))
+		if (renda == NaN) throw Cliente.erros.renda
+	}
+
+	static #validarEstadoCivil(estadoCivil) {
+
+	}
+
+}
+
 async function main() {
 	console.log("qual o numero de clientes?")
 	const qtdClientes = Number(await lerLinha())
